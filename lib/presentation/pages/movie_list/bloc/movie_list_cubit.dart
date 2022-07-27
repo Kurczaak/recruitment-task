4c478cddb84497fb4a521d5e85a330ca277992ab
kurczak <kurczakmichal97@gmail.com>
2022-07-27 21:25:32 +0200

    implemented search method

--- lib/presentation/pages/movie_list/bloc/movie_list_cubit.dart
+++ lib/presentation/pages/movie_list/bloc/movie_list_cubit.dart
@@ -10,6 +10,7 @@ import 'package:injectable/injectable.dart';
 @injectable
 class MovieListCubit extends Cubit<Result<List<MovieEntity>>> {
   MovieListCubit(this.searchMovieUseCase) : super(Result.initial());
+  static const debounceTime = Duration(milliseconds: 500);
 
   final SearchMovieUseCase searchMovieUseCase;
   final ScrollController scrollController = ScrollController();
@@ -22,12 +23,22 @@ class MovieListCubit extends Cubit<Result<List<MovieEntity>>> {
     emit(response);
   }
 
+  /// Searches for movies by [query] with no debouncing
+  void search({String query = ""}) async {
+    emit(Result.loading());
+
+    Result<List<MovieEntity>> response =
+        await searchMovieUseCase.searchMovie(query: query);
+    emit(response);
+  }
+
+  /// Searches for movies by [query] with the [debounceTime] specified in the cubit
   void instantSearch({String query = ""}) async {
     emit(Result.loading());
     if (timer != null) {
       timer!.cancel();
     }
-    timer = Timer(Duration(milliseconds: 500), () async {
+    timer = Timer(debounceTime, () async {
       Result<List<MovieEntity>> response =
           await searchMovieUseCase.searchMovie(query: query);
       emit(response);
